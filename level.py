f2845da771599474b086d327556000bdcd38ff53
Jorge Simoes <jorge.simoes@freiheit.com>
2024-01-27 01:32:01 +0100

    Add level class

---
+++ level.py
@@ -0,0 +1,37 @@
+import random
+import time
+
+class Level:
+    startLevelTime = None
+
+    def __init__(self, question, amountOfObject=None, initialTimer=60):
+        self.question = question
+        self.amountOfObject = amountOfObject
+        if amountOfObject is None:
+            self.amountOfObject = random.randint(0, 10)
+        self.initialTimer = initialTimer
+
+    def __str__(self):
+        return "question: " + self.question + "\nnumber of objects: " + str(self.amountOfObject) + "\ninitial timer(seconds): " + str(self.initialTimer)
+
+    def start(self):
+        self.startLevelTime = int(time.time())
+        return
+    
+    def isLevelFinished(self):
+        return int(time.time()) - self.startLevelTime >= self.initialTimer
+
+    def timeLeft(self):
+        return self.initialTimer - (int(time.time()) - self.startLevelTime)
+
+
+levelOne = Level("what is my age",2, 5)
+
+print(levelOne)
+
+levelOne.start()
+
+while levelOne.isLevelFinished() is not True:
+    time.sleep(1)
+    print("time remaining: " + str(levelOne.timeLeft()))
+
