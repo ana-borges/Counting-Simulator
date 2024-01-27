9ff60c48fa0e8c8b47674dd259387a41b1972878
Ana Borges <ana.agvb@gmail.com>
2024-01-27 01:37:54 +0000

    Reorganize code and check for keypresses

--- main-window.py
+++ main-window.py
@@ -7,7 +7,7 @@ if not pg.mixer:
     print("Warning, sound disabled")
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
-data_dir = os.path.join(main_dir, "data")
+data_dir = os.path.join(main_dir, "assets")
 
 def load_image(name, colorkey=None, scale=1):
     fullname = os.path.join(data_dir, name)
@@ -25,11 +25,11 @@ def load_image(name, colorkey=None, scale=1):
     return image, image.get_rect()
 
 class CountingObjects(pg.sprite.Sprite):
-    """moves the objects to be counted around the screen"""
+    """adds an object at a given position and with a given movement"""
 
     def __init__(self):
         pg.sprite.Sprite.__init__(self)  # call Sprite initializer
-        self.image, self.rect = load_image("diamond.png", -1, 4)
+        self.image, self.rect = load_image("diamond.jpg", -1, 0.4)
         screen = pg.display.get_surface()
         self.area = screen.get_rect()
         self.rect.topleft = 10, 90
@@ -45,37 +45,65 @@ class CountingObjects(pg.sprite.Sprite):
                 self.image = pg.transform.flip(self.image, True, False)
         self.rect = newpos
 
-# pygame setup
-pg.init()
-screen = pg.display.set_mode((1000, 1000), pg.SCALED)
-pg.display.set_caption("Counting Simulator")
-clock = pg.time.Clock()
-running = True
+def main():
+    """this function is called when the program starts.
+    it initializes everything it needs, then runs in
+    a loop until the function returns."""
+    # Initialize Everything
+    pg.init()
+    screen = pg.display.set_mode((1000, 1000), pg.SCALED)
+    pg.display.set_caption("Counting Simulator")
 
-background = pg.Surface(screen.get_size())
-background = background.convert()
-background.fill((102, 204, 10))
+    # Create The Background
+    background = pg.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((102, 204, 10))
 
-if pg.font:
-    font = pg.font.Font(None, 64)
-    text = font.render("How many sheep are there?", True, (10, 10, 10))
-    textpos = text.get_rect(centerx=background.get_width() / 2, y=background.get_height() - 100)
-    background.blit(text, textpos)
+    # Put Text On The Background, Centered
+    if pg.font:
+        font = pg.font.Font(None, 64)
+        text = font.render("How many objects are there?", True, (10, 10, 10))
+        textpos = text.get_rect(centerx=background.get_width() / 2, y=background.get_height() - 100)
+        background.blit(text, textpos)
 
-while running:
-    # poll for events
-    # pg.QUIT event means the user clicked X to close your window
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-
+    # Display The Background
     screen.blit(background, (0, 0))
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
     pg.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    # Prepare Game Objects
+    diamond = CountingObjects()
+    allobjects = pg.sprite.RenderPlain((diamond))
+    allobjects.draw(screen)
+    clock = pg.time.Clock()
 
-pg.quit()
+    # Main Loop
+    going = True
+    while going:
+        clock.tick(60)
+
+        # Handle Input Events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                going = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                # Close window if user presses ESC
+                going = False
+            elif event.type == pg.KEYDOWN:
+                print("A key! A key, I say!")
+
+        allobjects.update()
+
+        # Draw Everything
+        screen.blit(background, (0, 0))
+        allobjects.draw(screen)
+        pg.display.flip()
+
+    pg.quit()
+
+
+# Game Over
+
+
+# this calls the 'main' function when this script is executed
+if __name__ == "__main__":
+    main()
